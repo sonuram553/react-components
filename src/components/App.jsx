e80aae123907ac42e6d21c5b3fa3c2de7ebd2f97
Sonu Ram <sonuram6960@gmail.com>
2021-02-03 21:57:47 +0530

    Set context to test Tooltip

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,48 @@ import { Tooltip } from "./Tooltip";
 
 class App extends React.Component {
   render() {
-    return <Tooltip />;
+    return (
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae sed
+          labore hic maxime vitae quod voluptates, nostrum natus nam possimus,
+          dolor iusto, dolores alias provident facilis ea? Architecto, similique
+          veritatis?
+        </p>
+
+        <div className="container">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae,
+          veritatis quis tenetur consequuntur recusandae placeat. Nostrum culpa
+          eligendi incidunt eos hic earum modi illo! Nam voluptatem impedit
+          repudiandae pariatur explicabo.
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
+            voluptatem magni recusandae sapiente perferendis ab unde modi, neque
+            sequi veritatis optio soluta non, provident praesentium? Autem
+            obcaecati unde quibusdam dictaAA
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
+            voluptatem magni recusandae sapiente perferendis ab unde modi, neque
+            sequi veritatis optio soluta non, provident praesentium? Autem
+            obcaecati unde quibusdam dictaAA
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
+            voluptatem magni recusandae sapiente perferendis ab unde modi, neque
+            sequi veritatis optio soluta non, provident praesentium? Autem
+            obcaecati unde quibusdam dictaAA
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
+            voluptatem magni recusandae sapiente perferendis ab unde modi, neque
+            sequi veritatis optio soluta non, provident praesentium? Autem
+            obcaecati unde quibusdam dictaAA
+          </p>
+        </div>
+        <Tooltip />
+      </div>
+    );
   }
 }
 
